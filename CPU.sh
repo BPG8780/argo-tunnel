26a9f5cf0b68e79528ee105ce76f4f0d9ecc0bf9
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-06-07 12:20:17 +0800

    Add files via upload

--- CPU.sh
+++ CPU.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
-
-# 获取 CPU 利用率
-cpu_utilization=$(top -bn1 | grep "Cpu(s)" | \
-                   sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-                   awk '{print 100 - $1}')
-
-# 获取网络利用率
-net_utilization=$(ifstat 1 1 | tail -n 1 | \
-                   awk '{print $NF}' | cut -d '.' -f 1)
-
-# 获取内存利用率
-mem_utilization=$(free | awk '/Mem/{print $3/$2 * 100.0}')
-
-# 判断利用率是否符合要求
-if (( $(echo "$cpu_utilization < 15" | bc -l) )) &&
-   (( $(echo "$net_utilization < 15" | bc -l) )) &&
-   (( $(echo "$mem_utilization < 15" | bc -l) )); then
-    # 输出结果
-    echo "CPU 利用率为 $cpu_utilization%"
-    echo "网络利用率为 $net_utilization%"
-    echo "内存利用率为 $mem_utilization%"
-    echo "所有利用率都低于 15%。"
+if [ $EUID != 0 ]; then
+    echo "请以root用户权限运行此脚本"
+    exit 1
+fi
+if command -v python3 &>/dev/null; then
+    echo "Python 3 已经安装"
 else
-    echo "警告：至少有一个利用率高于 15%！"
+    echo "Python 3 未安装，正在安装..."    
+    # 安装 Python 3
+    if command -v dnf &>/dev/null; then          # 如果系统使用的是 dnf 包管理器
+        sudo dnf -y install python3
+    elif command -v yum &>/dev/null; then        # 如果系统使用的是 yum 包管理器
+        sudo yum -y install python3
+    elif command -v zypper &>/dev/null; then     # 如果系统使用的是 zypper 包管理器
+        sudo zypper -y install python3
+    elif command -v apt-get &>/dev/null; then    # 如果系统使用的是 apt-get 包管理器
+        sudo apt-get update
+        sudo apt-get -y install python3
+    else
+        echo "无法自动安装 Python 3，请手动安装。"   # 如果不支持任何包管理器，则提示需要手动安装
+        exit 1
+    fi    
+    # 设置 Python 3 为默认版本
+    if command -v update-alternatives &>/dev/null; then
+        sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+        sudo update-alternatives --set python /usr/bin/python3
+    elif command -v alternatives &>/dev/null; then
+        sudo alternatives --install /usr/bin/python python /usr/bin/python3 1
+        sudo alternatives --set python /usr/bin/python3
+    else
+        echo "无法自动设置 Python 3 为默认版本，请手动设置。"
+    fi
 fi
