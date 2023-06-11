1536e74f2952df17773ad4482084c1965265c72c
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-06-11 19:18:50 +0800

    Add files via upload

--- CPUM.sh
+++ CPUM.sh
@@ -13,4 +13,4 @@ cpu_speed=$(lscpu | grep 'CPU MHz:' | awk -F ': +' '{print $2}')
 echo -e "\033[1;33mCPU型号:\033[0m $cpu_model"
 echo -e "\033[1;33mCPU核心:\033[0m $num_cores"
 echo -e "\033[1;33mCPU速率:\033[0m ${cpu_speed}MHz"
-echo -e "\033[1;33mMEMOry:\033[0m ${mem_total_i}GB"
+echo -e "\033[1;33mMEMOry:\033[0m $mem_total_i G"
