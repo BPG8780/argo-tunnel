b641b633ab240ec56c445b8b0186e9a0cb32e4b5
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-06-11 19:08:40 +0800

    Add files via upload

--- CPUM.sh
+++ CPUM.sh
@@ -2,7 +2,7 @@
 
 # 获取 MemTotal 信息
 mem_total=$(grep 'MemTotal:' /proc/meminfo | awk '{print $2}')
-mem_total_g=$(numfmt --to=iec-i --suffix=B -d=1024G $mem_total)
+mem_total_h=$(numfmt --to=iec-i --suffix=B -d=1024G $mem_total)
 
 # 读取 CPU 核心数和其他信息
 num_cores=$(nproc)
@@ -10,7 +10,7 @@ cpu_model=$(lscpu | grep 'Model name:' | awk -F ': +' '{print $2}')
 cpu_speed=$(lscpu | grep 'CPU MHz:' | awk -F ': +' '{print $2}')
 
 # 打印输出结果（文本设置为黄色）
-echo -e "\033[1;33mCPU型号 :\033[0m $cpu_model"
-echo -e "\033[1;33mCPU核心 :\033[0m $num_cores"
-echo -e "\033[1;33mCPU速率 :\033[0m ${cpu_speed}MHz"
-echo -e "\033[1;33mMEMOry :\033[0m $mem_total_g"
+echo -e "\033[1;33mCPU型号:\033[0m $cpu_model"
+echo -e "\033[1;33mCPU核心:\033[0m $num_cores"
+echo -e "\033[1;33mCPU速率:\033[0m ${cpu_speed}MHz"
+echo -e "\033[1;33mMEMOry:\033[0m $mem_total_h"
