66d886f60a4a7b95e7a754eb50b7e69e9da2390c
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-06-11 18:53:21 +0800

    Add files via upload

--- CPUM.sh
+++ CPUM.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# 读取 CPU 核心数和内存信息
-num_cores=$(nproc)
-mem_info=$(free -h)
 
-# 从 lscpu 命令的输出中提取 CPU 型号和时钟频率
+# 获取 MemTotal 信息
+mem_total=$(grep 'MemTotal:' /proc/meminfo | awk '{print $2}')
+mem_total_h=$(numfmt --to=iec-i --suffix=B $mem_total)
+
+# 读取 CPU 核心数和其他信息
+num_cores=$(nproc)
 cpu_model=$(lscpu | grep 'Model name:' | awk -F ': +' '{print $2}')
 cpu_speed=$(lscpu | grep 'CPU MHz:' | awk -F ': +' '{print $2}')
 
@@ -11,5 +13,4 @@ cpu_speed=$(lscpu | grep 'CPU MHz:' | awk -F ': +' '{print $2}')
 echo -e "\033[1;33mCPU型号:\033[0m $cpu_model"
 echo -e "\033[1;33mCPU速率:\033[0m ${cpu_speed}MHz"
 echo -e "\033[1;33mCPU核心:\033[0m $num_cores"
-echo -e "\033[1;33m内存信息:\033[0m"
-echo -e "$mem_info"
+echo -e "\033[1;33m内存信息:\033[0m $mem_total_h"
