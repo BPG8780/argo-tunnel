6dd65ae0494aa6b649f0665f70dec43c4c47cda1
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 19:21:31 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,46 +1,81 @@
-latest_tag=$(curl -s "https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
+#!/bin/bash
 
-os_type=$(uname -s)
-if [ "$os_type" = "Linux" ]; then
-    if [ -f "/etc/debian_version" ]; then
-        os_name="Debian"
-    elif [ -f "/etc/lsb-release" ] && grep -q "DISTRIB_ID=Ubuntu" /etc/lsb-release; then
-        os_name="Ubuntu"
-    else
-        os_name=""
-    fi
+function install_bbrplus() {
+    latest_tag=$(curl -s "https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
 
-    if [ -n "$os_name" ]; then
-        math_version=${latest_tag:0:5}
-        arch=$(dpkg --print-architecture)
-        download="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/Debian-Ubuntu_Required_linux-image-$latest_tag"_"$math_version-1_$arch.deb"
-
-        wget -O "bbrplus.deb" $download
-
-        sudo dpkg -i bbrplus.deb
-
-        if [ $? -eq 0 ]; then
-            echo -e "\e[32mBBRPlus安装成功\e[0m"
-
-            if [ -e "/boot" ]; then
-                bbrplus_files=$(find /boot -name "*bbrplus*")
-                if [ -n "$bbrplus_files" ]; then
-                    echo -e "\e[32m在 boot 目录中找到 BBRPlus 内核文件\e[0m"
-                    echo "$bbrplus_files"
+    os_type=$(uname -s)
+    if [ "$os_type" = "Linux" ]; then
+        if [ -f "/etc/debian_version" ]; then
+            os_name="Debian"
+        elif [ -f "/etc/lsb-release" ] && grep -q "DISTRIB_ID=Ubuntu" /etc/lsb-release; then
+            os_name="Ubuntu"
+        else
+            os_name=""
+        fi
+    
+        if [ -n "$os_name" ]; then
+            math_version=${latest_tag:0:5}
+            arch=$(dpkg --print-architecture)
+            download="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/Debian-Ubuntu_Required_linux-image-$latest_tag"_"$math_version-1_$arch.deb"
+    
+            wget -O "bbrplus.deb" $download
+    
+            sudo dpkg -i bbrplus.deb
+    
+            if [ $? -eq 0 ]; then
+                echo -e "\e[32mBBRPlus安装成功\e[0m"
+    
+                if [ -e "/boot" ]; then
+                    bbrplus_files=$(find /boot -name "*bbrplus*")
+                    if [ -n "$bbrplus_files" ]; then
+                        echo -e "\e[32m在 boot 目录中找到 BBRPlus 内核文件\e[0m"
+                        echo "$bbrplus_files"
+                    else
+                        echo -e "\e[31m在 boot 目录中未找到 BBRPlus 内核文件，请检查您的安装。\e[0m"
+                    fi
                 else
-                    echo -e "\e[31m在 boot 目录中未找到 BBRPlus 内核文件，请检查您的安装。\e[0m"
+                    echo -e "\e[31m未找到 boot 目录，请检查您的安装。\e[0m"
                 fi
             else
-                echo -e "\e[31m未找到 boot 目录，请检查您的安装。\e[0m"
+                echo -e "\e[31mBBRPlus安装失败。\e[0m"
             fi
+    
+            rm bbrplus.deb
         else
-            echo -e "\e[31mBBRPlus安装失败。\e[0m"
+            echo -e "\e[31m该脚本仅适用于 Debian 和 Ubuntu 系统。\e[0m"
         fi
-
-        rm bbrplus.deb
     else
-        echo -e "\e[31m该脚本仅适用于 Debian 和 Ubuntu 系统。\e[0m"
+        echo -e "\e[31m该脚本仅适用于 Linux 系统。\e[0m"
     fi
-else
-    echo -e "\e[31m该脚本仅适用于 Linux 系统。\e[0m"
-fi
+}
+
+function display_menu() {
+    clear
+    echo "请选择一个选项："
+    echo "1. 安装 BBRPlus"
+    echo "2. 退出"
+    echo
+}
+
+function read_option() {
+    local choice
+    read -p "请输入您的选择: " choice
+    case $choice in
+        1)
+            install_bbrplus
+            ;;
+        2)
+            echo "正在退出..."
+            exit 0
+            ;;
+        *)
+            echo "无效的选择。"
+            ;;
+    esac
+}
+
+while true; do
+    display_menu
+    read_option
+    echo
+done
