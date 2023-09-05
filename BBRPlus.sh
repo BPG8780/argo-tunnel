3f9d642caa477b1caf7b4eb9464b41aba538e484
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 13:41:28 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,4 +1,27 @@
-version=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
-file_url=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | grep -oE "https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$version/Debian-Ubuntu_Required_linux-image-$version[^\"]*.deb")
+#!/bin/bash
+
+# 获取系统架构
+architecture=$(uname -m)
+
+# 使用curl命令从GitHub API获取发布信息，并提取最新的版本号
+version=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | jq -r ".tag_name")
+
+# 构建文件名模式
+filename_pattern="Debian-Ubuntu_Required_linux-image-$version"
+
+# 根据系统架构选择正确的后缀
+if [[ "$architecture" == "x86_64" ]]; then
+    filename_pattern="${filename_pattern}_amd64.deb"
+elif [[ "$architecture" == "aarch64" ]]; then
+    filename_pattern="${filename_pattern}_arm64.deb"
+else
+    echo "不支持的系统架构：$architecture"
+    exit 1
+fi
+
+# 使用curl命令从GitHub API获取文件列表，并通过jq过滤提取匹配的文件URL
+file_url=$(curl -s "https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest" | jq -r ".assets[] | select(.name | test(\"$filename_pattern\")) | .browser_download_url")
 filename=$(basename "$file_url")
+
+# 下载文件
 wget "$file_url" -O "$filename"
