937c112ccbb3dfcfb57ad470728d429f40c63e60
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 14:40:23 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# 发送HTTP请求获取页面内容
-response=$(curl -s "https://github.com/UJX6N/bbrplus-6.x_stable/releases/latest")
+# 获取最新版本号
+latest_version=$(curl -s https://github.com/UJX6N/bbrplus-6.x_stable/releases/latest | grep -oP '(?<=tag\/)[^"]+')
 
-# 从页面内容中提取最新版本号
-latest_version=$(echo "$response" | grep -oP '(?<=/tag/v)[^"]+' | head -n1)
-
+# 打印版本号
 echo "最新版本号为: $latest_version"
