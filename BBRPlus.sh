bdade506be0ae58ad3c58eb5b6484f47d1e14a94
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 14:53:08 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 URL=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \")
-OUTPUT_FILE="Debian-Ubuntu_Required_linux-image-latest_amd64.deb"
 
 # 下载文件
-wget "$URL" -O "$OUTPUT_FILE"
+curl -L -o $(basename "$URL") "$URL"
 
 # 输出下载完成信息
-echo "文件已下载到 $OUTPUT_FILE"
+echo "文件已下载到 $(basename "$URL")"
