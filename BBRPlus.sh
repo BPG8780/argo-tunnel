eec7c6cdac22f394c51f311b5fa055a210013c27
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 14:34:09 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# 获取最新版本号
-latest_version=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | grep '"tag_name":' | awk -F '"' '{print $4}')
+# 发送HTTP请求获取页面内容
+response=$(curl -s "https://github.com/UJX6N/bbrplus-6.x_stable/releases/latest")
 
-# 获取系统架构
-architecture=$(uname -m)
+# 从页面内容中提取最新版本号
+latest_version=$(echo "$response" | grep -oP '(?<=/tag/v)[^"]+' | head -n1)
 
-# 设置文件名模板
-file_template="Debian-Ubuntu_Required_linux-image-{{version}}-bbrplus_{{version}}-1_{{arch}}.deb"
-
-# 替换文件名模板中的版本号和架构
-file_name=$(echo "$file_template" | sed "s/{{version}}/$latest_version/g" | sed "s/{{arch}}/$architecture/g")
-
-# 下载文件
-curl -LO "https://github.com/UJX6N/bbrplus-6.x_stable/releases/latest/download/$file_name"
-
-# 重命名文件为bbrplus.deb
-mv "$file_name" "bbrplus.deb"
+echo "最新版本号为: $latest_version"
