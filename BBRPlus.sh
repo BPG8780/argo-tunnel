b9959fa075b439b34a6da4fcad31f14f5cff1741
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 13:32:50 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,24 +1,4 @@
-#!/bin/bash
-
-# 获取系统架构
-architecture=$(uname -m)
-
-# 使用curl命令从GitHub API获取发布信息，并提取最新的版本号
 version=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
-
-# 构建下载链接
-download_url="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$version/Debian-Ubuntu_Required_linux-image-$version"
-if [[ "$architecture" == "x86_64" ]]; then
-    download_url="$download_url\_amd64.deb"
-elif [[ "$architecture" == "aarch64" ]]; then
-    download_url="$download_url\_arm64.deb"
-else
-    echo "不支持的系统架构：$architecture"
-    exit 1
-fi
-
-# 提取文件名
-filename=$(basename "$download_url")
-
-# 下载文件
-wget "$download_url" -O "$filename"
+file_url=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | grep -oE "https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$version/Debian-Ubuntu_Required_linux-image-$version[^\"]*.deb")
+filename=$(basename "$file_url")
+wget "$file_url" -O "$filename"
