b54b1090ac91fa746cc20cd9299dda425449e866
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 16:13:18 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,7 +1,7 @@
 latest_tag=$(curl -s "https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
 
 # 将数学计算的结果赋值给变量math_version
-math_version=$(echo $latest_tag | awk -F'.' '{printf "%.1f\n", $1+$2/100}')
+math_version=${latest_tag:0:5}
 
 download_url="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/Debian-Ubuntu_Required_linux-image-$latest_tag-$math_version-1_amd64.deb"
 echo $download_url
