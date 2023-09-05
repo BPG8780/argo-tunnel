97bf5af70b8d40646fff664e803820d44aeae5e6
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 17:46:11 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -2,19 +2,23 @@ latest_tag=$(curl -s "https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/rele
 
 os_type=$(uname -s)
 if [ "$os_type" = "Linux" ]; then
-    if [ -f "/etc/debian_version" ] || [ -f "/etc/debian_release" ]; then
+    if [ -f "/etc/debian_version" ]; then
         os_name="Debian"
-    elif [ -f "/etc/lsb-release" ]; then
+    elif [ -f "/etc/lsb-release" ] && grep -q "DISTRIB_ID=Ubuntu" /etc/lsb-release; then
         os_name="Ubuntu"
     else
-        os_name="Linux"
+        os_name=""
+    fi
+
+    if [ -n "$os_name" ]; then
+        math_version=${latest_tag:0:5}
+        arch=$(dpkg --print-architecture)
+        download="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/Debian-Ubuntu_Required_linux-image-$latest_tag"_"$math_version-1_$arch.deb"
+
+        wget -O "bbrplus.deb" $download
+    else
+        echo "该脚本仅适用于 Debian 和 Ubuntu 系统."
     fi
 else
-    os_name="default"
+    echo "该脚本仅适用于 Linux 系统."
 fi
-
-math_version=${latest_tag:0:5}
-arch=$(dpkg --print-architecture)
-download="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/Debian-Ubuntu_Required_linux-image-$latest_tag"_"$math_version-1_$arch.deb"
-
-wget -O "bbrplus.deb" $download
