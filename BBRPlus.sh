5d1b28908153e6f3ed4d9174c730284912a7ad56
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 16:45:59 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,10 +1,22 @@
 latest_tag=$(curl -s "https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
 
-# 将数学计算的结果赋值给变量math_version
+os_type=$(uname -s)
+if [ "$os_type" = "Linux" ]; then
+    if [ -f "/etc/debian_version" ] || [ -f "/etc/debian_release" ]; then
+        os_name="Debian"
+    elif [ -f "/etc/lsb-release" ]; then
+        os_name="Ubuntu"
+    else
+        os_name="Linux"
+    fi
+else
+    os_name="default"
+fi
+
 math_version=${latest_tag:0:5}
 
-download_url="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/Debian-Ubuntu_Required_linux-image-$latest_tag-$math_version-1_amd64.deb"
+download_url="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/Debian-Ubuntu_Required_linux-image-$latest_tag_$math_version-1_amd64.deb"
 echo $download_url
 
 # 在这里添加需要的操作，比如下载文件等
-wget $download_url  # 下载文件到当前目录（需要安装wget命令）
+wget -O "Debian-Ubuntu_Required_linux-image_$latest_tag-$math_version-1_amd64.deb" $download_url  # 下载文件到当前目录（需要安装wget命令）
