c4fdd34859a23ee2220d85b766c223feecb90a3c
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-05 15:23:07 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
-URL=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \")
+# 检查系统类型
+if [[ "$(uname)" == "Linux" ]]; then
+    # 获取系统信息
+    if [[ -f /etc/os-release ]]; then
+        source /etc/os-release
+        OS=$NAME
+    elif type lsb_release >/dev/null 2>&1; then
+        OS=$(lsb_release -si)
+    elif [[ -f /etc/lsb-release ]]; then
+        source /etc/lsb-release
+        OS=$DISTRIB_ID
+    elif [[ -f /etc/debian_version ]]; then
+        OS=Debian
+    else
+        OS=$(uname -s)
+    fi
 
-# 下载文件
-curl -L -o $(basename "$URL") "$URL"
+    # 检查系统架构
+    ARCH=$(uname -m)
 
-# 输出下载完成信息
-echo "文件已下载到 $(basename "$URL")"
+    # 设置基本的URL地址
+    BASE_URL="https://github.com/UJX6N/bbrplus-6.x_stable/releases/latest/"
+
+    # 根据系统类型和架构拼接下载链接
+    DOWNLOAD_URL="${BASE_URL}"
+    case "$OS" in
+        "Ubuntu")
+            if [[ "$ARCH" == "x86_64" ]]; then
+                DOWNLOAD_URL+="Debian-Ubuntu_Required_linux-image-6.5.1-bbrplus_6.5.1-1_amd64.deb"
+            elif [[ "$ARCH" == "aarch64" ]]; then
+                DOWNLOAD_URL+="Debian-Ubuntu_Required_linux-image-6.5.1-bbrplus_6.5.1-1_arm64.deb"
+            fi
+            ;;
+        "CentOS")
+            if [[ "$ARCH" == "x86_64" ]]; then
+                DOWNLOAD_URL+="CentOS-7_Required_kernel-6.5.1-bbrplus.el7.x86_64.rpm"
+            elif [[ "$ARCH" == "aarch64" ]]; then
+                DOWNLOAD_URL+="CentOS-Stream-8_Required_kernel-6.5.1-bbrplus.el8.aarch64.rpm"
+            fi
+            ;;
+        *)
+            echo "Unsupported Linux distribution."
+            exit 1
+            ;;
+    esac
+
+    # 下载文件
+    if [[ -n "$DOWNLOAD_URL" ]]; then
+        wget "$DOWNLOAD_URL"
+    else
+        echo "Unsupported architecture."
+        exit 1
+    fi
+else
+    echo "This script is only compatible with Linux."
+    exit 1
+fi
