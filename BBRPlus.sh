6291f53d6f1d53da9fea73b4dfe8c8bcdf498ad9
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-06 18:34:18 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -44,7 +44,7 @@ function install_bbrplus() {
                             download="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/CentOS-8_Required_kernel-$latest_tag.el8.$arch.rpm"
                             ;;
                         *)
-                            echo -e "\e[31m该脚本不支持此架构。\e[0m"
+                            echo "该脚本不支持此架构。"
                             return
                             ;;
                     esac
@@ -52,7 +52,7 @@ function install_bbrplus() {
                     package_file="bbrplus.rpm"
                     ;;
                 *)
-                    echo -e "\e[31m该脚本仅适用于 Debian、Ubuntu、CentOS7 和 CentOS 8 系统。\e[0m"
+                    echo "该脚本仅适用于 Debian、Ubuntu、CentOS7 和 CentOS 8 系统。"
                     return
                     ;;
             esac
@@ -61,28 +61,28 @@ function install_bbrplus() {
             sudo $package_manager -i "$package_file"
 
             if [ $? -eq 0 ]; then
-                echo -e "\e[32mBBRPlus安装成功\e[0m"
+                echo "BBRPlus安装成功"
                 sleep 3
                 if [ -e "/boot" ]; then
                     bbrplus_files=$(find /boot -name "*bbrplus*")
                     if [ -n "$bbrplus_files" ]; then
-                        echo -e "\e[32m在 boot 目录中找到 BBRPlus 内核文件\e[0m"
+                        echo "在 boot 目录中找到 BBRPlus 内核文件"
                         echo "$bbrplus_files"
                     else
-                        echo -e "\e[31m在 boot 目录中未找到 BBRPlus 内核文件，请检查您的安装。\e[0m"
+                        echo "在 boot 目录中未找到 BBRPlus 内核文件，请检查您的安装。"
                     fi
                 else
-                    echo -e "\e[31m未找到 boot 目录，请检查您的安装。\e[0m"
+                    echo "未找到 boot 目录，请检查您的安装。"
                 fi
             else
-                echo -e "\e[31mBBRPlus安装失败。\e[0m"
+                echo "BBRPlus安装失败。"
                 sleep 3
             fi
 
             rm "$package_file"
             ;;
         *)
-            echo -e "\e[31m该脚本仅适用于 Linux 系统。\e[0m"
+            echo "该脚本仅适用于 Linux 系统。"
             ;;
     esac
     
