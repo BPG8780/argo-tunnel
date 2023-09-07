cb937efea2374fe47df3c72fd84e02bc4487783c
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-07 08:23:28 +0800

    Add files via upload

--- BBRPlus.sh
+++ BBRPlus.sh
@@ -1,3 +1,8 @@
+if [ "$EUID" -ne 0 ]; then
+    echo -e "\e[31m请以root用户身份运行该脚本\e[0m"
+    exit
+fi
+
 function install_bbrplus() {
     latest_tag=$(curl -s "https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
 
@@ -12,12 +17,6 @@ function install_bbrplus() {
                 centos_version=$(grep -oE '[0-9]+\.[0-9]+' /etc/centos-release)
                 if [[ "$centos_version" == "7" ]]; then
                     os_name="CentOS7"
-                elif [[ "$centos_version" =~ ^8.* ]]; then
-                    if cat /etc/centos-release | grep -q "CentOS Stream"; then
-                        os_name="CentOS-Stream-8"
-                    else
-                        os_name="CentOS-8"
-                    fi
                 else
                     os_name=""
                 fi
@@ -37,22 +36,8 @@ function install_bbrplus() {
                     package_manager="rpm"
                     package_file="bbrplus.rpm"
                     ;;
-                CentOS-8|CentOS-Stream-8)
-                    arch=$(uname -m)
-                    case $arch in
-                        x86_64|aarch64)
-                            download="https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/$latest_tag/CentOS-8_Required_kernel-$latest_tag.el8.$arch.rpm"
-                            ;;
-                        *)
-                            echo "该脚本不支持此架构。"
-                            return
-                            ;;
-                    esac
-                    package_manager="rpm"
-                    package_file="bbrplus.rpm"
-                    ;;
                 *)
-                    echo "该脚本仅适用于 Debian、Ubuntu、CentOS7 和 CentOS 8 系统。"
+                    echo -e "\e[31m该脚本仅适用于 Debian、Ubuntu 和 CentOS7 系统。\e[0m"
                     return
                     ;;
             esac
@@ -61,36 +46,34 @@ function install_bbrplus() {
             sudo $package_manager -i "$package_file"
 
             if [ $? -eq 0 ]; then
-                echo "BBRPlus安装成功"
+                echo -e "\e[32mBBRPlus安装成功\e[0m"
                 sleep 3
                 if [ -e "/boot" ]; then
                     bbrplus_files=$(find /boot -name "*bbrplus*")
                     if [ -n "$bbrplus_files" ]; then
-                        echo "在 boot 目录中找到 BBRPlus 内核文件"
+                        echo "在 boot 目录中找到 BBRPlus 内核文件:"
                         echo "$bbrplus_files"
                     else
-                        echo "在 boot 目录中未找到 BBRPlus 内核文件，请检查您的安装。"
+                        echo -e "\e[31m在 boot 目录中未找到 BBRPlus 内核文件，请检查您的安装。\e[0m"
                     fi
                 else
-                    echo "未找到 boot 目录，请检查您的安装。"
+                    echo -e "\e[31m未找到 boot 目录，请检查您的安装。\e[0m"
                 fi
             else
-                echo "BBRPlus安装失败。"
+                echo -e "\e[31mBBRPlus安装失败。\e[0m"
                 sleep 3
             fi
 
             rm "$package_file"
             ;;
         *)
-            echo "该脚本仅适用于 Linux 系统。"
+            echo -e "\e[31m该脚本仅适用于 Linux 系统。\e[0m"
             ;;
     esac
     
     sleep 20
 
-    if [ "$os_name" != "CentOS-Stream-8" ] && [ "$os_name" != "CentOS-8" ]; then
-        show_menu
-    fi
+    show_menu
 }
 
 
@@ -118,7 +101,7 @@ function display_menu() {
     echo "请选择一个选项："
     echo "1. 安装 BBRPlus"
     echo "2. 卸载 BBRPlus"
-    echo "3. 退出"
+    echo "0. 退出"
     echo
 }
 
