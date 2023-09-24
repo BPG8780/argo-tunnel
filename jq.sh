55dd336e201179f4da87f24698eab309f937fe7a
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 07:10:39 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# 发送GET请求并获取重定向地址
-redirect_url=$(curl -sL -w "%{url_effective}" -o /dev/null https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest)
+# 发送GET请求并获取JSON响应
+response=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest)
 
-# 提取下载链接
-download_url=$(echo "$redirect_url" | awk -F'/' '{print $NF}')
+# 使用jq解析JSON并获取符合条件的下载链接
+download_urls=$(echo "$response" | jq -r '.assets[].browser_download_url | select(contains("headers") | not)')
 
-# 打印下载链接
-echo "$download_url"
+# 遍历打印每个下载链接
+for url in $download_urls; do
+  echo "$url"
+done
