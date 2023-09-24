21957752e56049e3a63ad5c8b71ccf557ab64189
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 07:07:36 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# 发送HEAD请求并获取响应头部信息
-headers=$(curl -sI https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest)
+# 发送GET请求并获取重定向地址
+redirect_url=$(curl -sL -w "%{url_effective}" -o /dev/null https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest)
 
-# 使用grep过滤出包含"Location"的行，并提取下载链接
-download_urls=$(echo "$headers" | grep -i "^Location:" | awk '{print $2}')
+# 提取下载链接
+download_url=$(echo "$redirect_url" | awk -F'/' '{print $NF}')
 
-# 遍历打印每个下载链接
-for url in $download_urls; do
-  echo "$url"
-done
+# 打印下载链接
+echo "$download_url"
