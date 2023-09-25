480d7e91f8cb4aa618edbbb2cf7821c5ad272f8c
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-26 01:59:01 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -2,7 +2,6 @@
 
 API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 
-# 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
     DISTRIBUTION=$ID
@@ -16,22 +15,20 @@ else
     DISTRIBUTION=$(uname -s)
 fi
 
-# 特殊处理CentOS 7发行版
-if [[ $DISTRIBUTION == "centos" ]] && grep -q "release 7" /etc/centos-release; then
-    DISTRIBUTION="CentOS-7"
-fi
+case $DISTRIBUTION in
+    centos)
+        if grep -q "release 7" /etc/centos-release; then
+            DISTRIBUTION="CentOS-7"
+        fi
+        ;;
+    "Debian GNU/Linux"*)
+        DISTRIBUTION="Debian"
+        ;;
+    ubuntu)
+        DISTRIBUTION="Ubuntu"
+        ;;
+esac
 
-# 特殊处理Debian发行版
-if [[ $DISTRIBUTION == "Debian GNU/Linux"* ]]; then
-    DISTRIBUTION="Debian"
-fi
-
-# 特殊处理Ubuntu发行版
-if [[ $DISTRIBUTION == "ubuntu" ]]; then
-    DISTRIBUTION="Ubuntu"
-fi
-
-# 获取系统架构
 ARCHITECTURE=""
 if [[ $DISTRIBUTION == "Debian" || $DISTRIBUTION == "Ubuntu" ]]; then
     ARCHITECTURE=$(dpkg --print-architecture)
@@ -39,13 +36,10 @@ else
     ARCHITECTURE=$(uname -m)
 fi
 
-# 发送GET请求获取JSON数据
 response=$(curl -s "$API_URL")
 
-# 使用jq解析JSON数据
 download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '(.assets[] | select(.name | contains($distro) and contains($arch) and (contains("headers") | not))) | .browser_download_url')
 
-# 打印解析结果
 echo "Linux发行版: $DISTRIBUTION"
 echo "系统架构: $ARCHITECTURE"
 echo "下载链接: $download_url"
