079d6b1593d74cd28c723c72c5f4f38b08f0a191
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 08:40:09 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -6,7 +6,11 @@ response=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/release
 # 使用jq解析JSON并获取符合条件的下载链接（不包含 "headers"）
 download_urls=$(echo "$response" | jq -r '.assets[] | select(.name | contains("headers") | not) .browser_download_url')
 
-# 遍历打印每个下载链接
+# 遍历打印符合条件的下载链接
 for url in $download_urls; do
-  echo "$url"
+  if [[ $url == *"Debian-Ubuntu"* ]] && [[ $url == *"x86_64"* ]]; then
+    echo "适用于 Debian-Ubuntu x86_64 的链接: $url"
+  elif [[ $url == *"Debian-Ubuntu"* ]] && [[ $url == *"aarch64"* ]]; then
+    echo "适用于 Debian-Ubuntu aarch64 的链接: $url"
+  fi
 done
