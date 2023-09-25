d899788ae656c64b5389d996d36455b6c9c084eb
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-26 05:42:09 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -2,7 +2,6 @@
 
 API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 
-# 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
     DISTRIBUTION=$ID
@@ -16,41 +15,48 @@ else
     DISTRIBUTION=$(uname -s)
 fi
 
-# 获取系统架构
+case $DISTRIBUTION in
+    centos)
+        if grep -q "release 7" /etc/centos-release; then
+            DISTRIBUTION="CentOS-7"
+            INSTALL_COMMAND="rpm -i"
+        elif grep -q "release 8" /etc/centos-release; then
+            DISTRIBUTION="CentOS-Stream-8"
+            INSTALL_COMMAND="rpm -i"
+        fi
+        ;;
+    "Debian GNU/Linux"*)
+        DISTRIBUTION="Debian"
+        INSTALL_COMMAND="dpkg -i"
+        ;;
+    ubuntu)
+        DISTRIBUTION="Ubuntu"
+        INSTALL_COMMAND="dpkg -i"
+        ;;
+esac
+
 ARCHITECTURE=""
-if command -v dpkg &> /dev/null; then
+if [[ $DISTRIBUTION == "Debian" || $DISTRIBUTION == "Ubuntu" ]]; then
     ARCHITECTURE=$(dpkg --print-architecture)
 else
     ARCHITECTURE=$(uname -m)
 fi
 
-# 发送GET请求获取JSON数据
 response=$(curl -s "$API_URL")
 
-# 使用jq解析JSON数据
 download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '(.assets[] | select(.name | contains($distro) and contains($arch) and (contains("headers") | not))) | .browser_download_url')
 
-# 打印解析结果
-echo "Linux发行版: $DISTRIBUTION"
-echo "系统架构: $ARCHITECTURE"
-echo "开始下载..."
-curl -LO "$download_url"
+echo "正在下载BBR-PLUS..."
+wget "$download_url"
 
-# 重命名文件为bbrplus并保留后缀
 filename=$(basename "$download_url")
 extension="${filename##*.}"
 mv "$filename" "bbrplus.$extension"
 
-# 安装软件包
-echo "开始安装..."
-if command -v rpm &> /dev/null; then
-    rpm -i "bbrplus.$extension"
-elif command -v dpkg &> /dev/null; then
-    dpkg -i "bbrplus.$extension"
-else
-    echo "未找到适用于 $DISTRIBUTION 的安装命令。"
+echo "正在安装BBR-PLUS..."
+if [[ $INSTALL_COMMAND ]]; then
+    $INSTALL_COMMAND "bbrplus.$extension"
 fi
 
-# 删除下载的文件
-echo "删除下载的文件..."
+echo "正在删除下载的BBR-PLUS文件..."
 rm -f "bbrplus.$extension"
