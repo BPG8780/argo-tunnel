565f372bfe56725ab6d92994e879f03f94a90ee9
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-26 02:36:21 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -2,6 +2,7 @@
 
 API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 
+# 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
     DISTRIBUTION=$ID
@@ -19,6 +20,8 @@ case $DISTRIBUTION in
     centos)
         if grep -q "release 7" /etc/centos-release; then
             DISTRIBUTION="CentOS-7"
+        elif grep -q "release 8" /etc/centos-release; then
+            DISTRIBUTION="CentOS-Stream-8"
         fi
         ;;
     "Debian GNU/Linux"*)
@@ -29,6 +32,7 @@ case $DISTRIBUTION in
         ;;
 esac
 
+# 获取系统架构
 ARCHITECTURE=""
 if [[ $DISTRIBUTION == "Debian" || $DISTRIBUTION == "Ubuntu" ]]; then
     ARCHITECTURE=$(dpkg --print-architecture)
@@ -36,10 +40,13 @@ else
     ARCHITECTURE=$(uname -m)
 fi
 
+# 发送GET请求获取JSON数据
 response=$(curl -s "$API_URL")
 
+# 使用jq解析JSON数据
 download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '(.assets[] | select(.name | contains($distro) and contains($arch) and (contains("headers") | not))) | .browser_download_url')
 
+# 打印解析结果
 echo "Linux发行版: $DISTRIBUTION"
 echo "系统架构: $ARCHITECTURE"
 echo "下载链接: $download_url"
