e175df2932005daf5fbea765f1ee573fad8c55b1
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 08:44:36 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -8,9 +8,13 @@ download_urls=$(echo "$response" | jq -r '.assets[] | select(.name | contains("h
 
 # 遍历打印符合条件的下载链接
 for url in $download_urls; do
-  if [[ $url == *"Debian-Ubuntu"* ]] && [[ $url == *"x86_64"* ]]; then
-    echo "适用于 Debian-Ubuntu x86_64 的链接: $url"
-  elif [[ $url == *"Debian-Ubuntu"* ]] && [[ $url == *"aarch64"* ]]; then
-    echo "适用于 Debian-Ubuntu aarch64 的链接: $url"
+  if [[ $url == *"Ubuntu"* ]] && [[ $url == *"x86_64"* ]]; then
+    echo "适用于 Ubuntu x86_64 的链接: $url"
+  elif [[ $url == *"Ubuntu"* ]] && [[ $url == *"aarch64"* ]]; then
+    echo "适用于 Ubuntu aarch64 的链接: $url"
+  elif [[ $url == *"Debian"* ]] && [[ $url == *"x86_64"* ]]; then
+    echo "适用于 Debian x86_64 的链接: $url"
+  elif [[ $url == *"Debian"* ]] && [[ $url == *"aarch64"* ]]; then
+    echo "适用于 Debian aarch64 的链接: $url"
   fi
 done
