7398a7f77f87a550e24e2b2c16426c99d8ff2885
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 17:33:01 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -3,18 +3,15 @@
 # 发送GET请求并获取JSON响应
 response=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest)
 
-# 使用jq解析JSON并获取符合条件的下载链接（不包含 "headers"）
-download_urls=$(echo "$response" | jq -r '.assets[] | select(.name | contains("headers") | not) .browser_download_url')
+# 使用jq解析JSON并获取符合条件的下载链接
+download_urls=$(echo "$response" | jq -r '.assets[].browser_download_url | select(contains("headers") | not)')
+
+# 获取当前发行版信息
+distro=$(lsb_release -is)
 
 # 遍历打印符合条件的下载链接
 for url in $download_urls; do
-  if [[ $url == *"Ubuntu"* ]] && [[ $url == *"x86_64"* ]]; then
-    echo "适用于 Ubuntu x86_64 的链接: $url"
-  elif [[ $url == *"Ubuntu"* ]] && [[ $url == *"aarch64"* ]]; then
-    echo "适用于 Ubuntu aarch64 的链接: $url"
-  elif [[ $url == *"Debian"* ]] && [[ $url == *"x86_64"* ]]; then
-    echo "适用于 Debian x86_64 的链接: $url"
-  elif [[ $url == *"Debian"* ]] && [[ $url == *"aarch64"* ]]; then
-    echo "适用于 Debian aarch64 的链接: $url"
+  if [[ $distro == "Ubuntu" || $distro == "Debian" ]]; then
+    echo "适用于 $distro 的链接: $url"
   fi
 done
