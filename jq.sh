ff543d4c0a31540e40bf5e8674fe601db3dab03c
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 22:56:03 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -2,32 +2,32 @@
 
 API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 
-# 发送GET请求获取JSON数据
-response=$(curl -s "$API_URL")
-
 # 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
-    DISTRIBUTION=$NAME
+    DISTRIBUTION=$ID
 elif [ -f /usr/lib/os-release ]; then
     source /usr/lib/os-release
-    DISTRIBUTION=$NAME
-elif [ -f /etc/centos-release ]; then
-    source /etc/centos-release
+    DISTRIBUTION=$ID
+elif [ -f /etc/lsb-release ]; then
+    source /etc/lsb-release
     DISTRIBUTION=$DISTRIB_ID
 else
     DISTRIBUTION=$(uname -s)
 fi
 
 # 特殊处理Debian发行版
-if [[ $DISTRIBUTION == "Debian GNU/Linux"* ]]; then
+if [[ $DISTRIBUTION == "debian" ]]; then
     DISTRIBUTION="Debian"
 fi
 
 # 特殊处理CentOS 7发行版
-if [[ $DISTRIBUTION == "CentOS Linux release 7"* ]]; then
-    DISTRIBUTION="CentOS-7"
+if [[ $DISTRIBUTION == "centos" ]] && [ -f /etc/centos-release ] && grep -q "release 7" /etc/centos-release; then
+    DISTRIBUTION="CentOS"
 fi
+
+# 发送GET请求获取JSON数据
+response=$(curl -s "$API_URL")
 
 # 使用jq解析JSON数据
 download_urls=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" '.assets[] | select(.name | contains($distro)) | .browser_download_url')
