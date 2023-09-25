4ef93670d6b0892d5a9249fae300d4d35e60f479
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-26 03:42:53 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -2,7 +2,6 @@
 
 API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 
-# 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
     DISTRIBUTION=$ID
@@ -36,7 +35,6 @@ case $DISTRIBUTION in
         ;;
 esac
 
-# 获取系统架构
 ARCHITECTURE=""
 if [[ $DISTRIBUTION == "Debian" || $DISTRIBUTION == "Ubuntu" ]]; then
     ARCHITECTURE=$(dpkg --print-architecture)
@@ -44,27 +42,25 @@ else
     ARCHITECTURE=$(uname -m)
 fi
 
-# 发送GET请求获取JSON数据
 response=$(curl -s "$API_URL")
 
-# 使用jq解析JSON数据
 download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '(.assets[] | select(.name | contains($distro) and contains($arch) and (contains("headers") | not))) | .browser_download_url')
 
-# 打印解析结果
-echo "Linux发行版: $DISTRIBUTION"
-echo "系统架构: $ARCHITECTURE"
-echo "开始下载..."
-curl -LO "$download_url"
+# echo "Linux发行版: $DISTRIBUTION"
+# echo "系统架构: $ARCHITECTURE"
+echo "正在下载BBR-PLUS..."
+wget "$download_url"
 
-# 重命名文件为bbrplus并保留后缀
 filename=$(basename "$download_url")
 extension="${filename##*.}"
 mv "$filename" "bbrplus.$extension"
 
-# 安装软件包
-echo "开始安装..."
+echo "正在安装BBR-PLUS..."
 if [[ $INSTALL_COMMAND ]]; then
-    $INSTALL_COMMAND "bbrplus.$extension"
+    $INSTALL_COMMAND "BBRPLUS.$extension"
 else
-    echo "未找到适用于 $DISTRIBUTION 的安装命令。"
+
 fi
+
+echo "正在删除下载的BBR-PLUS文件..."
+rm -f "BBRPLUS.$extension"
