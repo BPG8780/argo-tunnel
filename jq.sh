c936f2d292cd9a3e60bc069e042256b683193d4f
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 17:42:50 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -6,12 +6,25 @@ response=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/release
 # 使用jq解析JSON并获取符合条件的下载链接
 download_urls=$(echo "$response" | jq -r '.assets[].browser_download_url | select(contains("headers") | not)')
 
-# 获取当前发行版信息
-distro=$(lsb_release -is)
-
-# 遍历打印符合条件的下载链接
+# 遍历打印每个下载链接，并根据发行版进行输出
 for url in $download_urls; do
-  if [[ $distro == "Ubuntu" || $distro == "Debian" ]]; then
-    echo "适用于 $distro 的链接: $url"
+  # 检查链接是否包含 "Debian-Ubuntu"
+  if [[ $url == *"Debian-Ubuntu"* ]]; then
+    # 替换链接中的字符为 "Ubuntu"
+    ubuntu_url="${url/Debian-Ubuntu/Ubuntu}"
+    echo "适用于 Ubuntu 的链接: $ubuntu_url"
+  else
+    # 输出原始下载链接
+    echo "原始下载链接: $url"
+  fi
+  
+  # 检查链接是否包含 "Debian-Ubuntu"
+  if [[ $url == *"Debian-Ubuntu"* ]]; then
+    # 替换链接中的字符为 "Debian"
+    debian_url="${url/Debian-Ubuntu/Debian}"
+    echo "适用于 Debian 的链接: $debian_url"
+  else
+    # 输出原始下载链接
+    echo "原始下载链接: $url"
   fi
 done
