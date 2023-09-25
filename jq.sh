10e6154d891af6558e75effe47234f02bb57b7f4
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 23:38:35 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -32,15 +32,17 @@ if [[ $DISTRIBUTION == "ubuntu" ]]; then
 fi
 
 # 获取系统架构
-ARCHITECTURE=$(dpkg --print-architecture)
+# ARCHITECTURE=$(dpkg --print-architecture)
 
 # 发送GET请求获取JSON数据
 response=$(curl -s "$API_URL")
 
 # 使用jq解析JSON数据
-download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '.assets[] | select(.name | contains($distro) and contains($arch)) | .browser_download_url')
+# download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '.assets[] | select(.name | contains($distro) and contains($arch)) | .browser_download_url')
+
+download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch '.assets[] | select(.name | contains($distro) and contains($arch)) | .browser_download_url')
 
 # 打印解析结果
 echo "Linux发行版: $DISTRIBUTION"
-echo "系统架构: $ARCHITECTURE"
+# echo "系统架构: $ARCHITECTURE"
 echo "下载链接: $download_url"
