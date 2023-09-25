2d341cebc848ad56b6c61fae17799959649512b5
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 18:10:56 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -6,6 +6,9 @@ response=$(curl -s https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/release
 # 使用jq解析JSON并获取符合条件的下载链接
 download_urls=$(echo "$response" | jq -r '.assets[].browser_download_url | select(contains("headers") | not)')
 
+# 获取系统架构
+arch=$(dpkg --print-architecture)
+
 # 遍历打印每个下载链接，并读取链接中的字符
 for url in $download_urls; do
   echo "原始下载链接: $url"
@@ -17,13 +20,9 @@ for url in $download_urls; do
   # 获取基本名称
   basename="${filename%.*}"
 
-  # 将 "Ubuntu" 替换为变量以识别 Ubuntu 系统
+  # 将 "Ubuntu" 替换为系统架构以识别 Ubuntu 系统
   distro="Ubuntu"
-  basename="${basename/Ubuntu/$distro}"
+  basename="${basename/Ubuntu/$distro-$arch}"
 
-  # 将 "arm64" 替换为变量以识别架构
-  arch="arm64"
-  basename="${basename/arm64/$arch}"
-
-  echo "获取Ubuntu: $basename"
+  echo "基本名称: $basename"
 done
