a3f7945076c8b82824a4fc687c4f10000d6e9792
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 20:40:13 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -5,9 +5,6 @@ API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 # 发送GET请求获取JSON数据
 response=$(curl -s "$API_URL")
 
-# 使用jq解析JSON数据
-download_url=$(echo "$response" | jq -r '.assets[0].browser_download_url')
-
 # 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
@@ -22,10 +19,21 @@ else
     DISTRIBUTION=$(uname -s)
 fi
 
-# 识别系统架构
-ARCHITECTURE=$(uname -m)
+# 使用jq解析JSON数据
+download_url=""
+case $DISTRIBUTION in
+    "Ubuntu")
+        download_url=$(echo "$response" | jq -r '.assets[] | select(.name | contains("ubuntu")) | .browser_download_url')
+        ;;
+    "CentOS")
+        download_url=$(echo "$response" | jq -r '.assets[] | select(.name | contains("centos")) | .browser_download_url')
+        ;;
+    # 添加其他发行版的处理逻辑...
+    *)
+        echo "不支持的发行版: $DISTRIBUTION"
+        exit 1
+esac
 
 # 打印解析结果
-echo "下载链接: $download_url"
 echo "Linux发行版: $DISTRIBUTION"
-echo "系统架构: $ARCHITECTURE"
+echo "下载链接: $download_url"
