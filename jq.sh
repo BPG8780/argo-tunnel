01e9fa7e4888f8fef80c66401da7d51f13aac79e
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-25 19:09:15 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -10,18 +10,23 @@ download_urls=$(echo "$response" | jq -r '.assets[].browser_download_url | selec
 distro=$(lsb_release -is)
 arch=$(dpkg --print-architecture)
 
-# 遍历打印每个下载链接，并读取链接中的字符
-for url in $download_urls; do
-  echo "原始下载链接: $url"
+# 检查发行版名称是否为 "Ubuntu"
+if [ "$distro" = "Ubuntu" ]; then
+  # 遍历打印每个下载链接，并读取链接中的字符
+  for url in $download_urls; do
+    echo "原始下载链接: $url"
 
-  # 使用变量读取链接中的字符
-  filename=$(basename "$url")
+    # 使用变量读取链接中的字符
+    filename=$(basename "$url")
 
-  # 提取基本名称部分
-  basename=$(echo "$filename" | sed 's|.*/\([^/]*\)_.*|\1|')
+    # 提取基本名称部分
+    basename=$(echo "$filename" | sed 's|.*/\([^/]*\)_.*|\1|')
 
-  # 替换发行版名称和架构以识别不同系统和架构
-  basename="${basename/Ubuntu/$distro}"
+    # 替换发行版名称和架构以识别不同系统和架构
+    basename="${basename/Debian-Ubuntu/$distro-$arch}"
 
-  echo "基本名称: $basename"
-done
+    echo "基本名称: $basename"
+  done
+else
+  echo "只有在 Ubuntu 系统上才能显示基本名称。"
+fi
