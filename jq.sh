c8059a4891843ac8be50467756f5ddeeff8c7247
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-26 18:33:27 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -5,32 +5,9 @@ API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 if [ -f /etc/os-release ]; then
     source /etc/os-release
     DISTRIBUTION=$NAME
-elif [ -f /usr/lib/os-release ]; then
-    source /usr/lib/os-release
-    DISTRIBUTION=$NAME
 else
     DISTRIBUTION=$(uname -s)
 fi
-
-case $DISTRIBUTION in
-    centos)
-        if grep -q "release 7" /etc/centos-release; then
-            DISTRIBUTION="CentOS-7"
-            INSTALL_COMMAND="rpm -i"
-        elif grep -q "release 8" /etc/centos-release; then
-            DISTRIBUTION="CentOS-Stream-8"
-            INSTALL_COMMAND="rpm -i"
-        fi
-        ;;
-    "Debian GNU/Linux"*)
-        DISTRIBUTION="Debian"
-        INSTALL_COMMAND="dpkg -i"
-        ;;
-    ubuntu)
-        DISTRIBUTION="Ubuntu"
-        INSTALL_COMMAND="dpkg -i"
-        ;;
-esac
 
 ARCHITECTURE=""
 if [[ $DISTRIBUTION == "Debian" || $DISTRIBUTION == "Ubuntu" ]]; then
@@ -45,8 +22,3 @@ download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch 
 
 echo "正在下载BBR-PLUS..."
 wget "$download_url"
-
-# filename=$(basename "$download_url")
-# extension="${filename##*.}"
-# mv "$filename" "bbrplus.$extension"
-
