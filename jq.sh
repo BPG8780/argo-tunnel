83bd2b1acee5f03abc09fa8d25a53cf203b0f44f
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-26 18:08:24 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -2,45 +2,51 @@
 
 API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 
-# 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
     DISTRIBUTION=$NAME
 elif [ -f /usr/lib/os-release ]; then
     source /usr/lib/os-release
     DISTRIBUTION=$NAME
-elif [ -f /etc/lsb-release ]; then
-    source /etc/lsb-release
-    DISTRIBUTION=$DISTRIB_ID
 else
     DISTRIBUTION=$(uname -s)
 fi
 
-# 特殊处理CentOS 7发行版
-if [[ $DISTRIBUTION == "centos" ]] && grep -q "release 7" /etc/centos-release; then
-    DISTRIBUTION="CentOS"
+case $DISTRIBUTION in
+    centos)
+        if grep -q "release 7" /etc/centos-release; then
+            DISTRIBUTION="CentOS-7"
+            INSTALL_COMMAND="rpm -i"
+        elif grep -q "release 8" /etc/centos-release; then
+            DISTRIBUTION="CentOS-Stream-8"
+            INSTALL_COMMAND="rpm -i"
+        fi
+        ;;
+    "Debian GNU/Linux"*)
+        DISTRIBUTION="Debian"
+        INSTALL_COMMAND="dpkg -i"
+        ;;
+    ubuntu)
+        DISTRIBUTION="Ubuntu"
+        INSTALL_COMMAND="dpkg -i"
+        ;;
+esac
+
+ARCHITECTURE=""
+if [[ $DISTRIBUTION == "Debian" || $DISTRIBUTION == "Ubuntu" ]]; then
+    ARCHITECTURE=$(dpkg --print-architecture)
+else
+    ARCHITECTURE=$(uname -m)
 fi
 
-# 特殊处理Debian发行版
-if [[ $DISTRIBUTION == Debian\ GNU\/Linux* ]]; then
-    DISTRIBUTION="Debian"
-fi
-
-# 特殊处理Ubuntu发行版
-if [[ $DISTRIBUTION == "ubuntu" ]]; then
-    DISTRIBUTION="Ubuntu"
-fi
-
-# 获取系统架构
-ARCHITECTURE=$(dpkg --print-architecture)
-
-# 发送GET请求获取JSON数据
 response=$(curl -s "$API_URL")
 
-# 使用jq解析JSON数据
 download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '(.assets[] | select(.name | contains($distro) and contains($arch) and (contains("headers") | not))) | .browser_download_url')
 
-# 打印解析结果
-echo "Linux发行版: $DISTRIBUTION"
-echo "系统架构: $ARCHITECTURE"
-echo "下载链接: $download_url"
+echo "正在下载BBR-PLUS..."
+wget "$download_url"
+
+# filename=$(basename "$download_url")
+# extension="${filename##*.}"
+# mv "$filename" "bbrplus.$extension"
+
