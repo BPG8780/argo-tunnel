19fe25494b028c124402a8ab41f07d688bbaa92e
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-27 00:14:51 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -39,6 +39,11 @@ response=$(curl -s "$API_URL")
 
 download_url=$(echo "$response" | jq -r --arg distro "$DISTRIBUTION" --arg arch "$ARCHITECTURE" '(.assets[] | select(.name | contains($distro) and contains($arch) and (contains("headers") | not))) | .browser_download_url')
 
+if [[ -z "$download_url" ]]; then
+    echo "获取下载文件失败～已停止……"
+    exit 1
+fi
+
 echo "正在下载BBR-PLUS..."
 wget "$download_url"
 
@@ -48,3 +53,13 @@ extension="${filename##*.}"
 
 # 重命名文件为bbrplus并保留原始扩展名
 mv "$filename" "bbrplus.$extension"
+
+if [[ $DISTRIBUTION == "Debian" || $DISTRIBUTION == "Ubuntu" ]]; then
+    echo "正在安装BBR-PLUS..."
+    dpkg -i "bbrplus.$extension"
+elif [[ $DISTRIBUTION == "CentOS-7" || $DISTRIBUTION == "CentOS-Stream-8" ]]; then
+    echo "正在安装BBR-PLUS..."
+    rpm -i "bbrplus.$extension"
+else
+    echo "不支持的Linux系统: $DISTRIBUTION"
+fi
