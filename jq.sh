30a7dd487124c61dfc0c8dc571b14081f56875c1
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-09-26 16:43:52 +0800

    Add files via upload

--- jq.sh
+++ jq.sh
@@ -5,10 +5,10 @@ API_URL="https://api.github.com/repos/UJX6N/bbrplus-6.x_stable/releases/latest"
 # 识别Linux发行版
 if [ -f /etc/os-release ]; then
     source /etc/os-release
-    DISTRIBUTION=$ID
+    DISTRIBUTION=$NAME
 elif [ -f /usr/lib/os-release ]; then
     source /usr/lib/os-release
-    DISTRIBUTION=$ID
+    DISTRIBUTION=$NAME
 elif [ -f /etc/lsb-release ]; then
     source /etc/lsb-release
     DISTRIBUTION=$DISTRIB_ID
